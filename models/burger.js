eff18bee6ffd4efab9bc3c63f8318731b8da9d3d
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-08 10:04:53 -0500

    update to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,21 @@
-//importing the ORM to create the functions that will interact with the database
-var orm = require("../config/orm.js");
-
-var burger = {
-  selectAll: function(cb) {
-    orm.selectAll(function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  insertOne: function(burger, cb) {
-    orm.insertOne(burger, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(id, cb) {
-    orm.updateOne([id], function(res) {
-      cb(res);
-    });
-  }
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our todo is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      // defaultValue is a flag that defaults a new todos complete value to false if
+      // it isn't supplied one
+      defaultValue: false
+    }
+  });
+  return Burger;
 };
-
-// export for controller
-module.exports = burger;

3bc5d3387e099e3b87a2d956c25716eacb59c4fc
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-08 10:15:13 -0500

    update to models

--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
   var Burger = sequelize.define("Burger", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     burger_name: {
       type: DataTypes.STRING,
-      // AllowNull is a flag that restricts a todo from being entered if it doesn't
-      // have a text value
       allowNull: false,
-      // len is a validation that checks that our todo is between 1 and 140 characters
       validate: {
         len: [1, 140]
       }
     },
     devoured: {
       type: DataTypes.BOOLEAN,
-      // defaultValue is a flag that defaults a new todos complete value to false if
-      // it isn't supplied one
+      allowNull: false,
       defaultValue: false
     }
   });
